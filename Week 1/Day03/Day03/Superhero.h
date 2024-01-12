255599e72c337dec2f29e9773219924bae01646a
Garrett Girod <girodg@hotmail.com>
2024-01-12 09:36:30 -0500

    optional params

---
+++ Week 1/Day03/Day03/Superhero.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+class Superhero
+{
+public:
+	Superhero(std::string name = "Batman", std::string secret = "Bruce Wayne")
+	{
+
+	}
+
+private:
+	std::string name_, secret_;
+};
+
